1715e6973b1aec6500d4982495ae8138114b970f
Neil Mitchell <ndmitchell@gmail.com>
2017-04-06 07:54:14 +0100

    Convert to Hi instead of HI

--- src/Main.hs
+++ src/Main.hs
@@ -23,7 +23,7 @@ weedDirectory dir = do
     distDir <- (dir </>) . takeWhile (/= '\n') . fromStdout <$> cmd (Cwd dir) "stack path --dist-dir"
     print distDir
     dumpHis <- filter ((==) ".dump-hi" . takeExtension) <$> listFilesRecursive distDir
-    his <- mapM (fmap parseHI . readFile') dumpHis
+    his <- mapM (fmap parseHi . readFile') dumpHis
     let hi = mconcat his
     let importPackage = Set.fromList $ hiImportPackage hi
     let exportIdent = Set.fromList $ hiExportIdent hi
@@ -34,19 +34,19 @@ weedDirectory dir = do
 
 explicitPackage = Set.fromList $ words "base process filepath directory containers unordered-containers yaml vector text bytestring transformers cpphs cmdargs haskell-src-exts uniplate ansi-terminal extra js-flot refact"
 
-data HI = HI
+data Hi = Hi
     {hiExportIdent :: [String]
     ,hiImportPackage :: [String]
     ,hiImportModule :: [String]
     ,hiImportIdent :: [String]
     } deriving Show
 
-instance Monoid HI where
-    mempty = HI [] [] [] []
-    mappend (HI x1 x2 x3 x4) (HI y1 y2 y3 y4) = HI (x1++y1) (x2++y2) (x3++y3) (x4++y4)
+instance Monoid Hi where
+    mempty = Hi [] [] [] []
+    mappend (Hi x1 x2 x3 x4) (Hi y1 y2 y3 y4) = Hi (x1++y1) (x2++y2) (x3++y3) (x4++y4)
 
-parseHI :: String -> HI
-parseHI = foldMap f . repeatedly (\(x:xs) -> first (x:) $ span (" " `isPrefixOf`) xs) .  lines
+parseHi :: String -> Hi
+parseHi = foldMap f . repeatedly (\(x:xs) -> first (x:) $ span (" " `isPrefixOf`) xs) .  lines
     where
         f (x:xs) | Just x <- stripPrefix "exports:" x = mempty{hiExportIdent=concatMap words $ x:xs}
         f (x:xs) | Just x <- stripPrefix "module dependencies:" x = mempty{hiImportModule=concatMap words $ x:xs}

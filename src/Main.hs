a3b723e04e446c2b93be0ef5c4cb753016c0ad28
Neil Mitchell <ndmitchell@gmail.com>
2017-04-27 16:03:31 +0100

    Push all the warning computation out to Warnings

--- src/Main.hs
+++ src/Main.hs
@@ -5,22 +5,21 @@ module Main(main) where
 import Hi
 import Cabal
 import Stack
-import Util
 import Data.List.Extra
 import Data.Maybe
 import Data.Functor
-import Data.IORef
 import Data.Tuple.Extra
 import Control.Monad
 import System.Exit
 import System.IO.Extra
 import qualified Data.HashMap.Strict as Map
-import qualified Data.HashSet as Set
 import System.Directory.Extra
 import System.FilePath
 import System.Environment
 import System.Process
+import Warnings
 import Prelude
+
 
 
 main :: IO ()
@@ -53,8 +52,6 @@ runTest update = do
                 callProcess "diff" ["-u3",dir </> "old.txt", dir </> "new.txt"]
         exitFailure
 
-listLines = map ("  "++) . sort
-
 weedDirectory :: FilePath -> IO Int
 weedDirectory dir = do
     file <- do b <- doesDirectoryExist dir; return $ if b then dir </> "stack.yaml" else dir
@@ -63,78 +60,14 @@ weedDirectory dir = do
         file <- selectCabalFile $ dir </> x
         (file,) <$> parseCabal file
 
-    -- put it in an IORef so harder to "lose" error reports
-    errCount <- newIORef 0
-    let reportErrors xs = do
-            modifyIORef errCount (+ length (filter (" " `isPrefixOf`) xs))
-            -- I tried stderr, but the output gets interleaved
-            putStr $ unlines xs
-
-    forM_ cabals $ \(cabalFile, cabal@Cabal{..}) -> do
+    fmap sum $ forM cabals $ \(cabalFile, cabal@Cabal{..}) -> do
         (fileToKey, keyToHi) <- hiParseDirectory $ takeDirectory cabalFile </> stackDistDir
-        putStrLn $ "= Weeding " ++ cabalName ++ " ="
-        cabalSections <- return $ map (id &&& selectHiFiles fileToKey) cabalSections
-
-        -- detect files used in more than one group
-        let reused :: [(HiKey, ModuleName, [CabalSectionType])] =
-                filter ((> 1) . length . thd3) $
-                map (\((a,b),c) -> (a,b,c)) $
-                groupSort
-                [((x, hiModuleName $ keyToHi Map.! x), cabalSectionType sect) | (sect, (x1,x2)) <- cabalSections, x <- x1++x2]
-        if null reused then
-            putStrLn "No modules reused between components"
-        else
-            reportErrors $ concat
-                [ ("Reused between " ++ intercalate ", " (map show pkgs)) : listLines mods
-                | (pkgs, mods) <- groupSort $ map (thd3 &&& snd3) reused]
-        let shared = Set.fromList $ map fst3 reused
-        putStrLn ""
-
-        generalBad <- forM cabalSections $ \(CabalSection{..}, (external, internal)) -> do
-            putStrLn $ "== Weeding " ++ cabalName ++ ", " ++ show cabalSectionType ++ " =="
-            shared <- return $ Set.fromList [hiModuleName $ keyToHi Map.! x | x <- external ++ internal, x `Set.member` shared]
-            shared <- return Set.empty
-            (external, internal) <- return $ both (map (keyToHi Map.!)) (external, internal)
-
-            -- first go looking for packages that are not used
-            let bad = Set.fromList cabalPackages `Set.difference` Set.unions (map hiImportPackage $ external ++ internal)
-            if Set.null bad then
-                putStrLn "No weeds in the build-depends field"
-            else
-                reportErrors $ "Redundant build-depends entries:" : listLines (Set.toList bad)
-
-            -- now look for modules which are imported but not in the other-modules list
-            let imports = Map.fromList [(hiModuleName, Set.map identModule hiImportIdent) | Hi{..} <- external ++ internal]
-            let missing = Set.filter (not . isPathsModule) $
-                          Set.unions (Map.elems imports) `Set.difference`
-                          Set.fromList (Map.keys imports)
-            let excessive = Set.fromList (map hiModuleName internal) `Set.difference`
-                            reachable (\k -> maybe [] Set.toList $ Map.lookup k imports) (map hiModuleName external)
-            if Set.null missing && Set.null excessive then
-                putStrLn "No weeds in the other-modules field"
-            else do
-                unless (Set.null missing) $
-                    reportErrors $ "Missing other-modules entries:" : listLines (Set.toList missing)
-                unless (Set.null excessive) $
-                    reportErrors $ "Excessive other-modules entries:" : listLines (Set.toList excessive)
-
-
-            -- now see which things are defined in and exported out of the internals, but not used elsewhere or external
-            let publicAPI = Set.unions $ map hiExportIdentUnsupported external
-            let visibleInternals = Set.unions [Set.filter ((==) hiModuleName . identModule) $ hiExportIdentUnsupported hi | hi@Hi{..} <- internal]
-            -- if someone imports and exports something assume that isn't also a use (find some redundant warnings)
-            let usedAnywhere = Set.unions [hiImportIdent `Set.difference` hiExportIdent | Hi{..} <- external ++ internal]
-            let bad = visibleInternals `Set.difference` Set.union publicAPI usedAnywhere
-            let badPerModule = groupSort [(m,i) | Ident m i <- Set.toList bad]
-            let (generalBad, myBad) = partition (flip Set.member shared . fst) badPerModule
-            if null myBad then
-                putStrLn "No weeds in the module exports"
-            else
-                reportErrors $ concat
-                    [ ("Weeds exported from " ++ m) : listLines is | (m, is) <- myBad]
+        putStrLn $ "= Project " ++ cabalName ++ " ="
+        let warn = warnings (keyToHi Map.!) $ map (id &&& selectHiFiles fileToKey) cabalSections
+        forM_ (groupSortOn warningSections warn) $ \warn -> do
+            putStrLn $ "== Section " ++ intercalate ", " (map show $ warningSections $ head warn) ++ " =="
+            forM_ (groupOn warningMessage warn) $ \warn ->
+                putStrLn $ warningMessage $ head warn
+            print warn
             putStrLn ""
-            return (cabalSectionType, generalBad)
-
-        return ()
-
-    readIORef errCount
+        return $ length warn
---
+++ src/Warnings.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE TupleSections, RecordWildCards, NamedFieldPuns, ScopedTypeVariables #-}
+
+module Warnings(Warning(..), warnings) where
+
+import Hi
+import Cabal
+import Util
+import Data.List.Extra
+import Data.Tuple.Extra
+import qualified Data.HashSet as Set
+import qualified Data.HashMap.Strict as Map
+
+
+data Warning = Warning
+    {warningMessage :: String
+    ,warningSections :: [CabalSectionType]
+    ,warningPackage :: Maybe PackageName
+    ,warningModule :: Maybe ModuleName
+    ,warningIdentifier :: Maybe IdentName
+    } deriving Show
+
+data S = S
+    {hi :: HiKey -> Hi
+    ,sections :: [(CabalSection, ([HiKey], [HiKey]))]
+    }
+
+
+---------------------------------------------------------------------
+-- ACTUAL WARNING COMPUTATION
+
+warnings :: (HiKey -> Hi) -> [(CabalSection, ([HiKey], [HiKey]))] -> [Warning]
+warnings hi sections = map (\x -> x{warningSections = sort $ warningSections x}) $
+    warnReusedModuleBetweenSections s ++
+    warnRedundantPackageDependency s ++
+    warnIncorrectOtherModules s ++
+    warnUnusedExport s
+    where s = S{..}
+
+
+warnReusedModuleBetweenSections :: S -> [Warning]
+warnReusedModuleBetweenSections S{..} =
+    [ Warning "Module reused between two components" ss Nothing (Just $ hiModuleName $ hi m) Nothing
+    | (m, ss) <- groupSort [(x, cabalSectionType c) | (c, (x1,x2)) <- sections, x <- x1++x2]
+    , length ss > 1]
+
+
+warnRedundantPackageDependency :: S -> [Warning]
+warnRedundantPackageDependency S{..} =
+    [ Warning "Redundant build-depends entry"
+        [cabalSectionType] (Just p) Nothing Nothing
+    | (CabalSection{..}, (x1,x2)) <- sections
+    , let usedPackages = Set.unions $ map (hiImportPackage . hi) $ x1 ++ x2
+    , p <- Set.toList $ Set.fromList cabalPackages `Set.difference` usedPackages]
+
+
+warnIncorrectOtherModules :: S -> [Warning]
+warnIncorrectOtherModules S{..} = concat
+    [ [Warning "Missing other-modules entry" [cabalSectionType] Nothing (Just m) Nothing | m <- Set.toList missing] ++
+      [Warning "Excessive other-modules entry" [cabalSectionType] Nothing (Just m) Nothing | m <- Set.toList excessive]
+    | (CabalSection{..}, (external, internal)) <- sections
+    , let imports = Map.fromList [(hiModuleName, Set.map identModule hiImportIdent) | Hi{..} <- map hi $ external ++ internal]
+    , let missing =  Set.filter (not . isPathsModule) $
+                     Set.unions (Map.elems imports) `Set.difference`
+                     Set.fromList (Map.keys imports)
+    , let excessive = Set.fromList (map (hiModuleName . hi) internal) `Set.difference`
+                      reachable (\k -> maybe [] Set.toList $ Map.lookup k imports) (map (hiModuleName . hi) external)
+    ]
+
+
+warnUnusedExport :: S -> [Warning]
+warnUnusedExport S{..} =
+    [ Warning "Weeds exported" ss Nothing (Just $ hiModuleName $ hi m) (Just i)
+    | (m,(ss,is)) <- Map.toList unused, i <- Set.toList is]
+    where
+        unionsWith f = foldr (Map.unionWith f) Map.empty
+        -- important: for an identifer to be unused, it must be unused in all sections that use that key
+        unused = unionsWith (\(s1,i1) (s2,i2) -> (s1++s2, i1 `Set.intersection` i2))
+                 [ Map.fromList [(k, ([cabalSectionType], Set.fromList $ Map.lookupDefault [] (hiModuleName $ hi k) bad)) | k <- internal]
+                 | (CabalSection{..}, (external, internal)) <- sections
+                 , let bad = Map.fromListWith (++) $ map (identModule &&& pure . identName) $ notUsedOrExposed (map hi external) (map hi internal)]
+
+notUsedOrExposed :: [Hi] -> [Hi] -> [Ident]
+notUsedOrExposed external internal = Set.toList $ privateAPI `Set.difference` Set.union publicAPI usedAnywhere
+    where
+        -- things exported from this package
+        publicAPI = Set.unions $ map hiExportIdent external
+
+        -- things that are defined in other modules and exported
+        privateAPI = Set.unions [Set.filter ((==) hiModuleName . identModule) $ hiExportIdentUnsupported hi | hi@Hi{..} <- internal]
+
+        -- things that are used anywhere, if someone imports and exports something assume that isn't also a use (find some redundant warnings)
+        usedAnywhere = Set.unions [hiImportIdent `Set.difference` hiExportIdent | Hi{..} <- external ++ internal]

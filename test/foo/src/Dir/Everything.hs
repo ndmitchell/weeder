22bd74e450e65c1c8a9ecbb57d13d6debc8bed9e
Neil Mitchell <ndmitchell@gmail.com>
2017-04-28 11:03:43 +0100

    Add a test for the signature parser

--- test/foo/src/Dir/Everything.hs
+++ test/foo/src/Dir/Everything.hs
@@ -3,7 +3,7 @@ module Dir.Everything(module Data.List.Extra, module Dir.Everything) where
 
 import Data.List.Extra
 
-usedFunction1 = 12 :: Int
+usedFunction1 = undefined :: (T1 -> T1) -> ()
 usedFunction2 = Other
 unusedFunction = 12 :: Int
 (=~=) a b = a == b -- used
@@ -17,6 +17,7 @@ data D2 = D2 {d2 :: Int}
 data D3 = D3 {d3 :: Int}
 data D4 = D4 {d4 :: Int}
 
+type T1 = D1
 
 data Other = Other
 

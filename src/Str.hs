8169192798188c08e4de0cb4eb80ce19f564829a
Neil Mitchell <ndmitchell@gmail.com>
2018-01-11 10:53:29 +0000

    Switch to normal foundation lines, now its faster

--- src/Str.hs
+++ src/Str.hs
@@ -10,8 +10,6 @@ module Str(
 import qualified Foundation as S
 import qualified Foundation.String as S
 import qualified Foundation.IO as S
-import qualified Foundation.Conduit as C
-import qualified Foundation.Conduit.Textual as C
 import Data.Tuple.Extra
 
 
@@ -20,16 +18,11 @@ type Str = S.String
 showLength :: S.CountOf a -> String
 showLength (S.CountOf x) = show x
 
-removeR :: Str -> Str
-removeR s | Just (s, c) <- S.unsnoc s, c == '\r' = s
-          | otherwise = s
-
 linesCR :: Str -> [Str]
-linesCR s = map removeR $ C.runConduitPure (C.yield s C..| C.lines C..| C.sinkList)
+linesCR = S.lines
 
 ugly :: S.Integral a => Integer -> a
 ugly = S.fromInteger
-
 
 readFileUTF8 :: FilePath -> IO Str
 readFileUTF8 = fmap (fst3 . S.fromBytes S.UTF8) . S.readFile . S.fromString

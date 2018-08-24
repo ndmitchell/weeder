84c7dd367a7146d98b67183f9247f6bc18cf987d
Neil Mitchell <ndmitchell@gmail.com>
2018-08-24 19:21:24 +0100

    On Travis avoid building self, which takes a while

--- travis.hs
+++ travis.hs
@@ -4,5 +4,4 @@ import System.Process.Extra
 main = do
     system_ "curl -sSL https://get.haskellstack.org/ | sh"
     system_ "stack init --resolver=nightly --ignore-subdirs --force"
-    system_ "weeder . --build --verbose +RTS -K1K"
     system_ "weeder --test --verbose +RTS -K1K"
